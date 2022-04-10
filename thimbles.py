98d70afc861687ac1b1a5da0781b69c6d99ba403
GordeyGudim <gordey.gudim@mail.ru>
2022-04-10 22:40:50 +0300

    thindles1.3

--- thimbles.py
+++ thimbles.py
@@ -4,25 +4,23 @@ def thimbles():
     return("Ben walks on a market. He have got 1000$. Ben sees man which shuffles a thimbles. Ben decides to approach for him...")
 def thimblerigger_regulations():
     return("Hey, Ben. It's game which called thimbles! I'm shuffle this timbles and you should guess where there is a ball")
-def base():
-    many = 1000
+def base(many=1000):
     if many >= 100:
         print("I'm mix.....")
         time.sleep(3)
         rand = random.randint(1,3)
         n = int(input("I'm mixing"))
         if n == rand:
-            many+=100
-            otv = input(f"You win! Do you want continue? Now you have got {many}$! y/n ")
+
+            otv = input(f"You win! Do you want continue? Now you have got {many+100}$! y/n ")
             if otv == "y":
-                return game()
+                return base(many=many+100)
             else:
                 return "Good buy"
         else:
-            many-=100
-            otv = input(f"You not win! Do you want continue? Now you have got {many}$! y/n ")
+            otv = input(f"You not win! Do you want continue? Now you have got {many-100}$! y/n ")
             if otv == "y":
-                return base()
+                return base(many=many-100)
             else:
                 return "Good buy, Ben!"
     else:
